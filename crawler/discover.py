de222f7443262fd12e6135bf51dea33e7a22efd7
Guilherme GM <321.guilherme@gmail.com>
2017-11-26 17:41:54 -0200

    update split_links function to check the rest lists

--- crawler/discover.py
+++ crawler/discover.py
@@ -3,7 +3,9 @@ from utils import helper
 from repository import db
 
 product_url_list = []
+product_checked_url_list = []
 resting_url_list = []
+resting_checked_url_list = []
 
 def start(SITE_URL):
     next_url = SITE_URL
@@ -14,18 +16,27 @@ def start(SITE_URL):
 
         site_content = http.get_site_content(next_url)
         links_found = http.get_links(site_content, SITE_URL)
-        helper.split_links(links_found, '/produto/', product_url_list, resting_url_list)
-        next_url = resting_url_list.pop(0)
+        helper.split_links(
+            links_found,
+            '/produto/',
+            product_url_list,
+            product_checked_url_list,
+            resting_url_list,
+            resting_checked_url_list
+        )
+        next_url = resting_url_list.pop()
+        print(next_url)
 
         while product_url_list:
-            product_url = product_url_list.pop(0)
+            product_url = product_url_list.pop()
+            print(product_url, len(product_url_list))
+
             product_details = http.get_product_details(
                 product_url,
                 'prodid: [\'"](.*?)[\'"]',
                 'pname: [\'"](.*?)[\'"]',
                 'value: [\'"](.*?)[\'"]'
             )
-            print(product_url)
             print(product_details)
             if product_details:
                 print(db.save_product(product_details))
--- utils/helper.py
+++ utils/helper.py
@@ -1,6 +1,15 @@
-def split_links(links, product_matcher, product_list, resting_list):
+def split_links(
+    links,
+    product_matcher,
+    product_url_list,
+    product_checked_url_list,
+    resting_url_list,
+    resting_checked_url_list
+):
     for link in links:
-        if link not in product_list and product_matcher in link:
-            product_list.append(link)
-        else:
-            resting_list.append(link)
+        if link not in product_url_list and link not in product_checked_url_list and product_matcher in link:
+            product_url_list.append(link)
+            product_checked_url_list.append(link)
+        elif link not in resting_checked_url_list:
+            resting_url_list.append(link)
+            resting_checked_url_list.append(link)
